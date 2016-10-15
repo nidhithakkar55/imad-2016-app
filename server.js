82043d67421c416d5d7972d710969e9e0e90da1d
nidhithakkar55 <nidhithakkar55@gmail.com>
2016-10-15 23:16:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/submit-name/:name', function(req, res) {
     
    var name = req.params.name;
    
-   name.push(name);
+   names.push(name);
    
    res.send(JSON.stringify(names));
 });
