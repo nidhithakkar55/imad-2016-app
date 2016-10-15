643d237bd8fa75014593839a09f2e1d04489d204
nidhithakkar55 <nidhithakkar55@gmail.com>
2016-10-15 19:34:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var articleOne = {
     title: 'Article One - Nidhi Thakkar',
     heading: 'Article One',
     date: 'Sep 15, 2016',
@@ -24,12 +24,52 @@ var content = {
             </p>`
 }
 
+function createTemplate (data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+            ${title}
+            </title>
+            
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+            
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+        
+    </html>
+    
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 }); 
 
 app.get('/article-two', function(req, res) {
